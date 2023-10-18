37ddef0265d25a59c2fb5492dd06aa158184bce6
choehojun <hj10271803@gmail.com>
2023-10-18 10:53:18 +0900

    Add clean function of build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@ elif [ ${sanitizer} == "ubsan" ]; then
 	emcc ./source/wasm_attack.c -fsanitize=undefined -o wasm_attack.html
 elif [ ${sanitizer} == "asan" ]; then
 	emcc ./source/wasm_attack.c -fsanitize=address -o wasm_attack.html
+elif [ ${sanitizer} == "clean" ]; then
+	rm *wasm*
+	exit 1
 else
-	echo "Please enter the type of sanitizer [none, ubsan, asan]"
+	echo "Please enter the type of sanitizer [none, ubsan, asan] or word \"clean\""
 	exit 1
 fi
 
